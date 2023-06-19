6b57200ff7195de5d15f6ad9f25ded3e299e245d
atglenn <adam.glenn@gmail.com>
2023-06-18 22:29:57 -0400

    Updates.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const { Liquid } = require("liquidjs");
+const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("stylesheets/styles.css");
     eleventyConfig.addPassthroughCopy("admin/config.yml");
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
     let options = {
         extname: ".liquid",

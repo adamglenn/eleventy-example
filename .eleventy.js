2d009fd5944b2c441f7112caa4abede705751a92
atglenn <adam.glenn@gmail.com>
2023-06-21 21:34:53 -0400

    Testing 11ty image plugin.

--- .eleventy.js
+++ .eleventy.js
@@ -2,89 +2,11 @@ const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const { Liquid } = require("liquidjs");
 const yaml = require("js-yaml");
 const Image = require("@11ty/eleventy-img");
-const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
 const { eleventyImagePlugin } = require("@11ty/eleventy-img");
-
-const stringifyAttributes = (attributeMap) => {
-  return Object.entries(attributeMap)
-  .map(([attribute, value]) => {
-    if (typeof value === 'undefined') return '';
-    return `${attribute}="${value}"`;
-  })
-  .join(' ');
-};
-
-const outdent = require('outdent');
-
-const imageShortcode = async (
-  src,
-  alt,
-  className = undefined,
-  widths = [400, 800, 1280],
-  formats = ['webp', 'jpeg'],
-  sizes = '100vw'
-) => {
-  const imageMetadata = await Image(src, {
-    widths: [...widths, null],
-    formats: [...formats, null],
-    outputDir: '_site/assets/img',
-    urlPath: '/assets/img',
-  });
-
-  const sourceHtmlString = Object.values(imageMetadata)
-  // Map each format to the source HTML markup
-  .map((images) => {
-    // The first entry is representative of all the others
-    // since they each have the same shape
-    const { sourceType } = images[0];
-
-    // Use our util from earlier to make our lives easier
-    const sourceAttributes = stringifyAttributes({
-      type: sourceType,
-      // srcset needs to be a comma-separated attribute
-      srcset: images.map((image) => image.srcset).join(', '),
-      sizes,
-    });
-
-    // Return one <source> per format
-    return `<source ${sourceAttributes}>`;
-  })
-  .join('\n');
-
-  const getLargestImage = (format) => {
-    const images = imageMetadata[format];
-    return images[images.length - 1];
-  }
-
-  const largestUnoptimizedImg = getLargestImage(formats[0]);
-  
-  const imgAttributes = stringifyAttributes({
-    src: largestUnoptimizedImg.url,
-    width: largestUnoptimizedImg.width,
-    height: largestUnoptimizedImg.height,
-    alt,
-    loading: 'lazy',
-    decoding: 'async',
-  });
-  
-  const imgHtmlString = `<img ${imgAttributes}>`;
-
-  const pictureAttributes = stringifyAttributes({
-    class: className,
-  });
-
-  const picture = `<picture ${pictureAttributes}>
-    ${sourceHtmlString}
-    ${imgHtmlString}
-  </picture>`;
-
-  return outdent`${picture}`;
-};
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("stylesheets/styles.css");
   eleventyConfig.addPassthroughCopy("admin/config.yml");
-  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
@@ -98,7 +20,38 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setLibrary("liquid", new Liquid(options));
 
-  eleventyConfig.addShortcode('image', imageShortcode);
+  eleventyConfig.addShortcode("thumbnailImage", async function(src, alt) {
+		if(alt === undefined) {
+			// You bet we throw an error on missing alt (alt="" works okay)
+			throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+		}
+
+		let metadata = await Image(src, {
+			widths: [960],
+			formats: ["webp"]
+		});
+
+		let data = metadata.webp[metadata.webp.length - 1];
+		return `<img src="${data.url}" alt="${alt}" loading="lazy" decoding="async">`;
+	});
+
+  eleventyConfig.addShortcode("bgImage", async function(src, alt) {
+		if(alt === undefined) {
+			// You bet we throw an error on missing alt (alt="" works okay)
+			throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+		}
+
+		let metadata = await Image(src, {
+			widths: [960, 1920],
+			formats: ["webp", "jpeg"]
+		});
+
+		let data = metadata.webp[metadata.webp.length - 1];
+		return `style="background-image:url('${data.url}');`;
+	});
+
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("img");
 
   return {
     htmlTemplateEngine: "liquid"
